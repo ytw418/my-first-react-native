1e404690fc01a51e8d157f3d8f96c9b97bbd594e
ytw418 <ytw418@gmail.com>
2021-08-30 14:53:22 +0900

    채팅앱 빌드 성공

--- chapter09/react-native-simple-chat/src/utils/firebase.js
+++ chapter09/react-native-simple-chat/src/utils/firebase.js
@@ -1,90 +1,108 @@
-// import * as firebase from 'firebase';
-// import 'firebase/firestore';
-// import config from '../../firebase.json';
+import firebase from 'firebase/app'
+import 'firebase/firestore';
 
-// const app = firebase.initializeApp(config);
+import 'firebase/storage'; 
 
-// const Auth = app.auth();
+// Add the Firebase products that you want to use
+import "firebase/auth";
 
-// export const login = async ({ email, password }) => {
-//   const { user } = await Auth.signInWithEmailAndPassword(email, password);
-//   return user;
-// };
 
-// const uploadImage = async uri => {
-//   const blob = await new Promise((resolve, reject) => {
-//     const xhr = new XMLHttpRequest();
-//     xhr.onload = function () {
-//       resolve(xhr.response);
-//     };
-//     xhr.onerror = function (e) {
-//       reject(new TypeError('Network request failed'));
-//     };
-//     xhr.responseType = 'blob';
-//     xhr.open('GET', uri, true);
-//     xhr.send(null);
-//   });
+const firebaseConfig = {
+    apiKey: "AIzaSyCNCHREJk1YH6FYd4_aW1pUl-KpkcaZbdE",
+    authDomain: "react-native-f6c42.firebaseapp.com",
+    databaseURL: "https://react-native-f6c42-default-rtdb.firebaseio.com",
+    projectId: "react-native-f6c42",
+    storageBucket: "react-native-f6c42.appspot.com",
+    messagingSenderId: "781325592216",
+    appId: "1:781325592216:web:88eac6dba0cda0f4d152ed",
+    measurementId: "G-1L99G8X9C3"
+  };
 
-//   const user = Auth.currentUser;
-//   const ref = app.storage().ref(`/profile/${user.uid}/photo.png`);
-//   const snapshot = await ref.put(blob, { contentType: 'image/png' });
 
-//   blob.close();
-//   return await snapshot.ref.getDownloadURL();
-// };
 
-// export const signup = async ({ email, password, name, photoUrl }) => {
-//   const { user } = await Auth.createUserWithEmailAndPassword(email, password);
-//   const storageUrl = photoUrl.startsWith('https')
-//     ? photoUrl
-//     : await uploadImage(photoUrl);
-//   await user.updateProfile({
-//     displayName: name,
-//     photoURL: storageUrl,
-//   });
-//   return user;
-// };
+const app = firebase.initializeApp(firebaseConfig);
 
-// export const logout = async () => {
-//   return await Auth.signOut();
-// };
+const Auth = app.auth();
 
-// export const getCurrentUser = () => {
-//   const { uid, displayName, email, photoURL } = Auth.currentUser;
-//   return { uid, name: displayName, email, photoUrl: photoURL };
-// };
+export const login = async ({ email, password }) => {
+  const { user } = await Auth.signInWithEmailAndPassword(email, password);
+  return user;
+};
 
-// export const updateUserPhoto = async photoUrl => {
-//   const user = Auth.currentUser;
-//   const storageUrl = photoUrl.startsWith('https')
-//     ? photoUrl
-//     : await uploadImage(photoUrl);
-//   await user.updateProfile({ photoURL: storageUrl });
-//   return { name: user.displayName, email: user.email, photoUrl: user.photoURL };
-// };
+const uploadImage = async uri => {
+  const blob = await new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+    xhr.onerror = function (e) {
+      reject(new TypeError('Network request failed'));
+    };
+    xhr.responseType = 'blob';
+    xhr.open('GET', uri, true);
+    xhr.send(null);
+  });
 
-// export const DB = firebase.firestore();
+  const user = Auth.currentUser;
+  const ref = app.storage().ref(`/profile/${user.uid}/photo.png`);
+  const snapshot = await ref.put(blob, { contentType: 'image/png' });
 
-// export const createChannel = async ({ title, description }) => {
-//   const newChannelRef = DB.collection('channels').doc();
-//   const id = newChannelRef.id;
-//   const newChannel = {
-//     id,
-//     title,
-//     description,
-//     createdAt: Date.now(),
-//   };
-//   await newChannelRef.set(newChannel);
-//   return id;
-// };
+  blob.close();
+  return await snapshot.ref.getDownloadURL();
+};
 
-// export const createMessage = async ({ channelId, message }) => {
-//   return await DB.collection('channels')
-//     .doc(channelId)
-//     .collection('messages')
-//     .doc(message._id)
-//     .set({
-//       ...message,
-//       createdAt: Date.now(),
-//     });
-// };
+export const signup = async ({ email, password, name, photoUrl }) => {
+  const { user } = await Auth.createUserWithEmailAndPassword(email, password);
+  const storageUrl = photoUrl.startsWith('https')
+    ? photoUrl
+    : await uploadImage(photoUrl);
+  await user.updateProfile({
+    displayName: name,
+    photoURL: storageUrl,
+  });
+  return user;
+};
+
+export const logout = async () => {
+  return await Auth.signOut();
+};
+
+export const getCurrentUser = () => {
+  const { uid, displayName, email, photoURL } = Auth.currentUser;
+  return { uid, name: displayName, email, photoUrl: photoURL };
+};
+
+export const updateUserPhoto = async photoUrl => {
+  const user = Auth.currentUser;
+  const storageUrl = photoUrl.startsWith('https')
+    ? photoUrl
+    : await uploadImage(photoUrl);
+  await user.updateProfile({ photoURL: storageUrl });
+  return { name: user.displayName, email: user.email, photoUrl: user.photoURL };
+};
+
+export const DB = firebase.firestore();
+
+export const createChannel = async ({ title, description }) => {
+  const newChannelRef = DB.collection('channels').doc();
+  const id = newChannelRef.id;
+  const newChannel = {
+    id,
+    title,
+    description,
+    createdAt: Date.now(),
+  };
+  await newChannelRef.set(newChannel);
+  return id;
+};
+
+export const createMessage = async ({ channelId, message }) => {
+  return await DB.collection('channels')
+    .doc(channelId)
+    .collection('messages')
+    .doc(message._id)
+    .set({
+      ...message,
+      createdAt: Date.now(),
+    });
+};
